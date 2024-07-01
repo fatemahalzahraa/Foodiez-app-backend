38a14bab98773ae82396a3f3e5d498066cf7f756
A <alzemamia@gmail.com>
2024-07-02 00:57:25 +0300

    fixed a typo

--- api/users/controllers.js
+++ api/users/controllers.js
@@ -31,7 +31,7 @@ const register = async (req, res, next) => {
 const login = async (req, res, next) => {
   try {
     const user = req.user;
-    const token = generateToken(newUser);
+    const token = generateToken(user);
     return res.status(200).json({ token });
   } catch (error) {
     next(error);
